52b0419cca73a2b833723d0147a3387fe655640e
jnsmtnr <janosmaitner@gmail.com>
2022-01-27 19:19:05 +0100

    day 10

---
+++ day10/puzzle20.js
@@ -0,0 +1,27 @@
+let seq = '1113122113'
+
+for (let i = 0; i < 50; i++) {
+    let newSeq = ''
+    
+    let current = ''
+    let number = 0
+    
+    for (const c of seq) {
+        if (!current) {
+            current = c
+            number = 1
+        }
+        else if (current === c) number++
+        else {
+            newSeq += number.toString() + current
+            current = c
+            number = 1
+        }
+    }
+    
+    newSeq += number.toString() + current
+    
+    seq = newSeq
+}
+
+console.log(seq.length)
