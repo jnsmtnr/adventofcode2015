f60b439f5a7e4b08317d57be5ccc8780af6e2361
jnsmtnr <janosmaitner@gmail.com>
2022-01-29 09:57:55 +0100

    day 11

---
+++ day11/puzzle21.js
@@ -0,0 +1,55 @@
+const abc = 'abcdefghijklmnopqrstuvwxyz'
+
+const password = 'hepxcrrq'.split('')
+
+while (true) {
+    let first = false
+
+    for (let i = 0; i < 6; i++) {
+        if (
+            abc.indexOf(password[i]) + 1 === abc.indexOf(password[i + 1]) &&
+            abc.indexOf(password[i + 1]) + 1 === abc.indexOf(password[i + 2])
+        ) {
+            first = true
+            break
+        }
+    }
+
+    let second = false
+    let pairs = 0
+
+    for (let i = 0; i < 7; i++) {
+        if (password[i] === password[i + 1]) {
+            pairs++
+            i++
+        }
+    }
+
+    second = pairs >= 2
+
+    let third = true
+
+    for (let i = 0; i < 8; i++) {
+        if (['i', 'o', 'l'].includes(password[i])) {
+            third = false
+            break
+        }
+    }
+
+    if (first && second && third) {
+        console.log(password.join(''))
+        break
+    }
+
+    for (let i = password.length - 1; i >= 0; i--) {
+        if (password[i] !== 'z') {
+            password[i] = abc[abc.indexOf(password[i]) + 1]
+            break
+        }
+        else {
+            password[i] = 'a'
+        }
+    }
+}
+
+module.exports = { password: password.join('') }
